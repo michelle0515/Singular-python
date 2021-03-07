dffe8106208ba15ff7f67603f2de043ec6f8dc7a
michelle0515 <michellesu070515@gmail.com>
2021-03-07 11:33:30 +0800

    sprint

---
+++ 20210307.py
@@ -0,0 +1,16 @@
+#print(1+2+3+4+5)
+#print('hello world')
+#print(1*2-3/4)
+#pi=3.14
+#print(pi)
+#print
+
+input('你今天food panda了嗎？')
+print('哈')
+input('今天的心情如何')
+print('耶～')
+input('今天天氣好嗎？)
+print('喔耶～')
+
+
+
