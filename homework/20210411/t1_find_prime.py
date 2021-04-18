cebdda82c63ae7cf0d0fc5774b0a26a49b53511a
michelle0515 <80183593+michelle0515@users.noreply.github.com>
2021-04-18 09:52:45 +0800

    Update t1_find_prime.py

--- homework/20210411/t1_find_prime.py
+++ homework/20210411/t1_find_prime.py
@@ -6,11 +6,12 @@
 """
 #a=int(input('number:'))
 #for i in range(1,a+1):
+
 a=int(input('number:'))
-for i in range(2,a):
-    for r in range(1,i):
-     if i%r==0:
-        continue
-     else:
+for i in range(2,a+1):
+    for r in range(2,i):
+      if i%r==0:
+        break
+    else:
         print(i)
 
