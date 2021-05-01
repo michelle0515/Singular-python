c491d06c5dda436ec3fd1268b6ef38d98e1ee6ef
michelle0515 <80183593+michelle0515@users.noreply.github.com>
2021-05-01 21:55:31 +0800

    Update t1_lcm.py

--- homework/20210425/t1_lcm.py
+++ homework/20210425/t1_lcm.py
@@ -3,3 +3,19 @@
 並使用function def 求最小公倍數
 value = lcm(num1, num2)
 '''
+def lcm(x,y):
+    if x>y:
+        a=x
+    else:
+        a=y
+    while True:
+        if a%x==0 and a%y==0:
+            lcm=a
+            break
+        a+=1
+
+    return lcm
+
+num1=int(input('number1:'))
+num2=int(input('number2:'))
+print(num1,'and',num2,'最小公倍數為:',lcm(num1,num2))
