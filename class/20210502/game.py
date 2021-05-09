ac2be491c8e851d30b66647efabd63194b5c8c39
michelle0515 <80183593+michelle0515@users.noreply.github.com>
2021-05-09 11:38:55 +0800

    class game

--- class/20210502/game.py
+++ class/20210502/game.py
@@ -1,18 +1,99 @@
 '''
- 初始血量:10點
- 初始錢:0元
- 冒險者攻擊力:1-3點
- 怪物攻擊力:1點
- 獲得紅藥水:1-3點血量
- 獲得錢袋:10-30元
- 擊敗怪物可以獲得10-20元
- 回合制，冒險者有先攻擊的優先權
- 冒險者隨機對怪物產生1-3點傷害
- 怪物生命隨機2-10點，攻擊力1點
- 戰鬥結束後，更新冒險者狀態
- status[0] = 1活著，0死亡
- status[1] = 剩餘生命
- status[2] = 剩餘金幣
- '''
+初始血量:10點
+初始錢:0元
+冒險者攻擊力:1-3點
+怪物攻擊力:1點
+獲得紅藥水:1-3點血量
+獲得錢袋:10-30元
+擊敗怪物可以獲得10-20元
+回合制，冒險者有先攻擊的優先權
+冒險者隨機對怪物產生1-3點傷害
+怪物生命隨機2-10點，攻擊力1點
+戰鬥結束後，更新冒險者狀態
+status[0] = 1活著，0死亡
+status[1] = 剩餘生命
+status[2] = 剩餘金幣
+'''
+import random
+def update_life(life):
+    get_life=random.randit(1,3)
+    new_life=life+get_life
+    print('RecoveryLife=%d Your life %d'%(get_life,new_life)
+    return new_life
+
+def update_money(money):
+    get_money=random.randit(10,20)
+    new_money=money+get_money
+    print('RecoveryMoney=%d Your money %d'%(get_money,new_money)
+    return new_money
+
+def fighting(life,money):
+    status=[0,0,0]
+    monster_life=random.randint(2,10)
+    print('Monster Life = %d'%monster_life)
+    while True:
+        attack=random.randit(1,3)
+        print('You Make Damage%d'%attack)
+        monster_life-=attack
+        time.sleep(1)
+        print('Monster Life %d'%monster_life)
+
+        if(monster_life<1):
+           print('You beat monster')
+           money+=random.randint(10,20)
+           status[0]=1
+           status[1]=life
+           status[2]=money
+          return status
+
+        print('Monster atteck')
+        life-=1
+        print('You get hurt,My life %d'%my_life)
+
+        if(my_life<1):
+           print('You dead')
+           status[0]=0
+           status[1]=life
+           status[2]=money
+    return status
+status=[1,10,0]
+while True:
+    rev=input("Do you want 'c' continue or 'q' quit the game？")
+    if(rev=='c'):
+       gen_event=random.randint(1,3)
+       if(gen_event==1):
+           sts[1]=life(sts[1])
+           if(gen_event==2):
+               sts[2]=money(sts[2])
+               if(gen_event==3):
+                   sts=fighting(sts[1],sts[2])
+                   if(sts[0]==0):
+                       print('game over')
+                       break
+                    print('sts=%s'%sts)
+    elif(rev==q):
+        print('bye')
+        break
+    else:
+        continue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- homework/20210502/t0_adventure-advenced.py
+++ homework/20210502/t0_adventure-advenced.py
@@ -5,3 +5,126 @@
   回魔藥水，一瓶100元/2-4點魔力，錢不夠無法購買
   回血藥水，一瓶100元/3-6點體力，錢不夠無法購買
 '''
+import random
+import time
+def update_life(status: list):
+    get_life = random.randint(1, 3)
+    status[1] += get_life
+    print("Recovry Life = %d Your life = %d" % (get_life, status[1]))
+    return status
+
+
+def update_money(status: list):
+    get_money = random.randint(10, 30)
+    status[2] += get_money
+    print("Get Money = %d Your Money = %d" % (get_money, status[2]))
+    return status
+
+
+def fighting(status: list):
+    s = 0.5  # attack speed
+    monster_life = random.randint(2, 10)
+    print("Monster Life = %d" % monster_life)
+
+    while True:
+        while True:
+            act = input("Use 'm' for magic,'n' for normal,'r' for reef: ")
+            if (act == 'm'):
+                if status[3] >= 1:
+                    attack = random.randint(4, 10) + status[4]
+                    status[3] -= 1
+                    break
+                else:
+                    print('not enough')
+                    attack = random.randint(1, 3) + status[4]
+                    break
+            elif act == 'n':
+                attack = random.randint(1, 3) + status[4]
+                break
+            elif act == 'r':
+                if status[5] >= 1:
+                    attack = monster_life
+                    status[5] -= 1
+                else:
+                    print('not enough')
+                    attack = random.randint(1, 3) + status[4]
+                    break
+
+
+        print("You make damage %d" % attack)
+        monster_life -= attack
+        time.sleep(s)
+        print("Monster Life %d" % monster_life)
+
+        if (monster_life < 1):
+            print("You beat monster")
+            status[2] += random.randint(10, 20)
+            return status
+        else:
+            print("Monster Attack")
+            status[1] -= 1
+            time.sleep(s)
+            print("You hurt, Life = %d" % status[1])
+
+            if (status[1] < 1):
+                print("You dead")
+                status[0] = 0
+                return status
+
+
+def store(status: list):
+    while True:
+        if (status[2] < 100):
+            print("Money is not enough, quit store ")
+            break
+
+        act = input("What do you want to buy 'l'/life,'m'/magic,'w'/weapon,'r'/reef,'q'/quit:")
+
+        if (act == 'q'):
+            print("88")
+            break
+        elif (act == 'l'):
+            status[1] += random.randint(1, 3)
+            status[2] -= 100
+            print("Life=%d, Money=%d" % (status[1], status[2]))
+        elif (act == 'm'):
+            status[3] += random.randint(1, 3)
+            status[2] -= 100
+            print("Magic=%d, Money=%d" % (status[3], status[2]))
+        elif (act == 'w'):
+            status[4] += random.randint(1, 3)
+            status[2] -= 100
+            print("Weapon=%d, Money=%d" % (status[4], status[2]))
+        elif (act == 'r'):
+            status[5] += random.randint(1, 3)
+            status[2] -= 500
+            print("Weapon=%d, Money=%d" % (status[4], status[2]))
+        else:
+            continue
+    return status
+
+#main program
+sts = [1, 10, 0, 10, 0, 0]
+event_list = [update_life, update_money, fighting, store]
+while True:
+    '''
+    rev = input("Do you want 'c' continue 'q' quit the game:")
+    if (rev == "c"):
+        sts = event_list[random.randint(0, len(event_list) - 1)](sts)
+        if (sts[0] == 0):
+            print("Game Over")
+            break
+        print("[[life = %d, money = %d, MP = %d]]" % (sts[1], sts[2], sts[3]))
+    elif (rev == "q"):
+        print("88")
+        break
+    else:
+        continue
+    '''
+    sts = event_list[random.randint(0, len(event_list) - 1)](sts)
+    if (sts[0] == 0):
+        print("Game Over")
+        break
+    print("[[life = %d, money = %d, MP = %d, W= %d]]" % (sts[1], sts[2], sts[3], sts[4]))
+    time.sleep(1)
+
