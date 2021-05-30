b5759bed3c4dca724bef205274c639a282fe260f
michelle0515 <80183593+michelle0515@users.noreply.github.com>
2021-05-30 10:13:21 +0800

    Update hw8_.py

--- homework/20210523/topic/hw8_.py
+++ homework/20210523/topic/hw8_.py
@@ -4,7 +4,7 @@
 01 start = input("你今天幾點開始工作?")
 02 end = input("現在幾點?")
 03
-如果要完成這個程式,在03行應使用哪個程式碼?B
+如果要完成這個程式,在03行應使用哪個程式碼?A
 A. print("你已經工作了" + str(int(end) - int(start)) + "小時!")
 B. print("你已經工作了" + (int(end) - int(start)) + "小時!")
 C. print("你已經工作了" + str(end - start) + "小時!")
