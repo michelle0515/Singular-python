cf89a7776454da9d90e006f21ad92b085cbbef12
michelle0515 <80183593+michelle0515@users.noreply.github.com>
2021-05-30 12:19:03 +0800

    Create 0530class.py

---
+++ class/20210530/0530class.py
@@ -0,0 +1,16 @@
+'''
+print('I %d savage' %94)
+print('I %3d savage' %94)
+print('I %03d savage' %94)
+'''
+'''
+print('I {0:d} savage'.format(94))
+print('I {0:3d} savage'.format(94))
+print('I {0:03d} savage'.format(94))
+'''
+'''
+print('I %s savage' %'94')
+print('I {0:s} savage' .format('94'))
+'''
+B=['a',True,8,False]
+print(8 in B)
